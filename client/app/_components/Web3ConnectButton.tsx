d489008513123da5f412814c77b1d3a9d0b3f3c6
theblockchaineth <36228038+theblockchaineth@users.noreply.github.com>
2024-07-09 16:38:13 -0400

    init

---
+++ client/app/_components/Web3ConnectButton.tsx
@@ -0,0 +1,3 @@
+export default function Web3ConnectButton() {
+    return <w3m-button />
+}
---
+++ client/app/page.tsx
@@ -0,0 +1,10 @@
+import Web3ConnectButton from "./_components/Web3ConnectButton";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <p> HELLO WORLD</p>
+      <Web3ConnectButton />
+    </main>
+  );
+}
